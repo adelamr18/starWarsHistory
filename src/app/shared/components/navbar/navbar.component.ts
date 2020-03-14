f06e35dd2a8dd6afa6548c02afcd8c3fa15a8478
Adel  Amr <adelgabreil@adels-mbp.localdomain>
2020-03-14 13:38:58 +0100

    Create view for navbar component and implement relevant logic for navigation

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,15 +1,86 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+import { CollectionsServiceService } from "../../services/collections-service.service";
+import { Angular2SwapiService } from "angular2-swapi";
+import { subTitles } from "../../constants/defines";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.css"]
 })
 export class NavbarComponent implements OnInit {
+  @Output() onGenreChange = new EventEmitter();
+  id: number;
+  data: any;
 
-  constructor() { }
+  constructor(
+    public collectionsService: CollectionsServiceService,
+    private swapi: Angular2SwapiService
+  ) {}
 
   ngOnInit(): void {
+    this.setFilmsSubtitles();
   }
 
+  navigateToFilms = () => {
+    this.swapi.getFilms(1).subscribe(res => {
+      res ? (this.data = res) : null;
+      this.setFilmsSubtitles();
+      this.onGenreChange.emit({
+        curruentData: res,
+        currentPath: "films"
+      });
+    });
+    this.collectionsService.genreType = subTitles.films;
+  };
+
+  navigateToCharacters = () => {
+    this.swapi.getPeople(1).subscribe(res => {
+      res ? (this.data = res) : null;
+      this.setCharactersSubtitles();
+      this.onGenreChange.emit({
+        curruentData: res,
+        currentPath: "characters"
+      });
+    });
+    this.collectionsService.genreType = subTitles.characters;
+  };
+
+  navigateToPlanets = () => {
+    this.swapi.getPlanets(1).subscribe(res => {
+      res ? (this.data = res) : null;
+      this.setPlanetsSubtitles();
+      this.onGenreChange.emit({
+        curruentData: res,
+        currentPath: "planets"
+      });
+    });
+    this.collectionsService.genreType = subTitles.planets;
+  };
+
+  setFilmsSubtitles = () => {
+    this.collectionsService.subTitleOne = subTitles.director;
+    this.collectionsService.subTitleTwo = subTitles.producer;
+    this.collectionsService.subTitleThree = subTitles.releaseDate;
+  };
+
+  setCharactersSubtitles = () => {
+    this.collectionsService.subTitleOne = subTitles.actorHeight;
+    this.collectionsService.subTitleTwo = subTitles.actorHairColor;
+    this.collectionsService.subTitleThree = subTitles.actorGender;
+  };
+
+  setPlanetsSubtitles = () => {
+    this.collectionsService.subTitleOne = subTitles.planetOrbitalPeriod;
+    this.collectionsService.subTitleTwo = subTitles.planetClimate;
+    this.collectionsService.subTitleThree = subTitles.planetGravity;
+  };
+
+  showDashboardImage = () => {
+    this.collectionsService.showDashboardImage = true;
+    this.onGenreChange.emit({
+      curruentData: "",
+      currentPath: ""
+    });
+  };
 }
