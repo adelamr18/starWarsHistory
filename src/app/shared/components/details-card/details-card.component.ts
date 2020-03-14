5fdff15a1320dbc59d5e7cf8ffe134b1b2fb9c69
Adel  Amr <adelgabreil@adels-mbp.localdomain>
2020-03-14 13:46:07 +0100

    Create view and implement logic needed to display detailed information cards and further detailed cards

--- src/app/shared/components/details-card/details-card.component.ts
+++ src/app/shared/components/details-card/details-card.component.ts
@@ -1,15 +1,73 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { CollectionsServiceService } from "../../services/collections-service.service";
+import { Router } from "@angular/router";
+import { Location } from "@angular/common";
+import { HttpClient } from "@angular/common/http";
+import { detailedCardSubtitles, subTitles } from "../../constants/defines";
 
 @Component({
-  selector: 'app-details-card',
-  templateUrl: './details-card.component.html',
-  styleUrls: ['./details-card.component.css']
+  selector: "app-details-card",
+  templateUrl: "./details-card.component.html",
+  styleUrls: ["./details-card.component.css"]
 })
 export class DetailsCardComponent implements OnInit {
-
-  constructor() { }
+  constructor(
+    public collectionService: CollectionsServiceService,
+    private router: Router,
+    private location: Location,
+    private http: HttpClient
+  ) {}
+  cardDetail: any;
+  detailedNames = [];
+  data: any;
+  homePlanet: any;
 
   ngOnInit(): void {
+    this.getCardDetails();
   }
 
+  onGenreChange = event => {
+    this.collectionService.currenentDisplayedCards = event.curruentData;
+    this.location.replaceState(event.currentPath);
+    this.collectionService.currentUrl = event.currentPath;
+    this.router.navigate([`/${event.currentPath}`]);
+  };
+
+  onFurtherDetailsRequest = event => {
+    this.collectionService.currentlySelectedCard = event;
+    event["name"] ? this.setCharacterFurtherDetails() : null;
+    event["title"] ? this.setCharacterFurtherDetails() : null;
+  };
+
+  setCharacterFurtherDetails = () => {
+    this.collectionService.cardDetailOne = detailedCardSubtitles.actorDetails;
+    this.collectionService.cardDetailTwo = subTitles.actorHeight;
+    this.collectionService.cardDetailThree = detailedCardSubtitles.weight;
+    this.collectionService.cardDetailFour = subTitles.actorHairColor;
+    this.collectionService.cardDetailFive =
+      detailedCardSubtitles.actorEyesColor;
+    this.collectionService.cardDetailSix = detailedCardSubtitles.actorBirthDate;
+    this.collectionService.cardDetailSeven = subTitles.actorGender;
+    this.collectionService.cardDetailEight =
+      detailedCardSubtitles.actorHomePlanet;
+    this.router.navigate(["/characterDetail", this.collectionService.id]);
+  };
+
+  setFilmFurtherDetails = () => {
+    this.collectionService.cardDetailOne = detailedCardSubtitles.filmDetails;
+    this.collectionService.cardDetailTwo = subTitles.director;
+    this.collectionService.cardDetailThree = subTitles.producer;
+    this.collectionService.cardDetailFour = subTitles.releaseDate;
+    this.collectionService.cardDetailFive = detailedCardSubtitles.episode;
+    this.router.navigate(["/filmDetail", this.collectionService.id]);
+  };
+
+  getCardDetails = () => {
+    this.cardDetail = this.collectionService.currentlySelectedCard;
+    if (this.cardDetail && this.cardDetail["homeworld"]) {
+      this.http.get(this.cardDetail.homeworld).subscribe(res => {
+        res ? (this.homePlanet = res) : null;
+      });
+    }
+  };
 }
--- src/app/shared/components/further-detailed-card/further-detailed-card.component.ts
+++ src/app/shared/components/further-detailed-card/further-detailed-card.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-further-detailed-card',
-  templateUrl: './further-detailed-card.component.html',
-  styleUrls: ['./further-detailed-card.component.css']
+  selector: "app-further-detailed-card",
+  templateUrl: "./further-detailed-card.component.html",
+  styleUrls: ["./further-detailed-card.component.css"]
 })
 export class FurtherDetailedCardComponent implements OnInit {
-
-  constructor() { }
+  constructor(private http: HttpClient) {}
+  data: any;
+  @Input() url: string = "";
+  @Input() curruentIndex: number;
+  @Output() onFurtherDetailsRequest = new EventEmitter();
 
   ngOnInit(): void {
+    this.getFurtherDetailsData();
   }
-
+  navigateToMoreDetails = () => {
+    this.onFurtherDetailsRequest.emit(this.data);
+  };
+  getFurtherDetailsData = () => {
+    this.http.get(this.url).subscribe(res => {
+      this.data = res;
+    });
+  };
 }
