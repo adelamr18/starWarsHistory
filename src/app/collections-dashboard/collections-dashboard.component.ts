10478c7c40e99107cb421969602a8cbca4076cc0
Adel  Amr <adelgabreil@adels-mbp.localdomain>
2020-03-14 13:42:45 +0100

    Connect navbar to dashboard collections and implement relevant logic to show cards for films,characters and planets

--- src/app/collections-dashboard/collections-dashboard.component.ts
+++ src/app/collections-dashboard/collections-dashboard.component.ts
@@ -1,15 +1,102 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { CollectionsServiceService } from "../shared/services/collections-service.service";
+import { Router } from "@angular/router";
+import { detailedCardSubtitles, subTitles } from "../shared/constants/defines";
+import { Location } from "@angular/common";
 
 @Component({
-  selector: 'app-collections-dashboard',
-  templateUrl: './collections-dashboard.component.html',
-  styleUrls: ['./collections-dashboard.component.css']
+  selector: "app-collections-dashboard",
+  templateUrl: "./collections-dashboard.component.html",
+  styleUrls: ["./collections-dashboard.component.css"]
 })
 export class CollectionsDashboardComponent implements OnInit {
-
-  constructor() { }
+  constructor(
+    public collectionsService: CollectionsServiceService,
+    private router: Router,
+    private location: Location
+  ) {}
+  collectionsData: any;
+  clickedCardId: number = 0;
 
   ngOnInit(): void {
+    this.checkUrlCurrentState();
   }
 
+  onGenreChange(event) {
+    event.curruentData
+      ? this.displayDashboardData(event)
+      : this.resetDashboard();
+  }
+
+  onInformationRequest = event => {
+    let selectedCardId = this.collectionsService.id++;
+    if (event.identifyCard === 1) {
+      this.setFilmDetailsInfo(event.cardData);
+      this.router.navigate(["/filmDetail", selectedCardId]);
+    }
+    if (event.identifyCard === 2) {
+      this.setCharacterDetailsInfo(event.cardData);
+      this.router.navigate(["/characterDetail", selectedCardId]);
+    }
+    if (event.identifyCard === 3) {
+      this.setPlanetDetailsInfo(event.cardData);
+      this.router.navigate(["/planetDetail", selectedCardId]);
+    }
+  };
+
+  setFilmDetailsInfo = cardData => {
+    this.collectionsService.currentlySelectedCard = cardData;
+    this.collectionsService.cardDetailOne = detailedCardSubtitles.filmDetails;
+    this.collectionsService.cardDetailTwo = subTitles.director;
+    this.collectionsService.cardDetailThree = subTitles.producer;
+    this.collectionsService.cardDetailFour = subTitles.releaseDate;
+    this.collectionsService.cardDetailFive = detailedCardSubtitles.episode;
+  };
+
+  setCharacterDetailsInfo = cardData => {
+    this.collectionsService.currentlySelectedCard = cardData;
+    this.collectionsService.cardDetailOne = detailedCardSubtitles.actorDetails;
+    this.collectionsService.cardDetailTwo = subTitles.actorHeight;
+    this.collectionsService.cardDetailThree = detailedCardSubtitles.weight;
+    this.collectionsService.cardDetailFour = subTitles.actorHairColor;
+    this.collectionsService.cardDetailFive =
+      detailedCardSubtitles.actorEyesColor;
+    this.collectionsService.cardDetailSix =
+      detailedCardSubtitles.actorBirthDate;
+    this.collectionsService.cardDetailSeven = subTitles.actorGender;
+    this.collectionsService.cardDetailEight =
+      detailedCardSubtitles.actorHomePlanet;
+  };
+
+  setPlanetDetailsInfo = cardData => {
+    this.collectionsService.currentlySelectedCard = cardData;
+    this.collectionsService.cardDetailOne = detailedCardSubtitles.planetDetails;
+    this.collectionsService.cardDetailTwo = detailedCardSubtitles.planetType;
+    this.collectionsService.cardDetailThree =
+      detailedCardSubtitles.planetRotationPeriod;
+    this.collectionsService.cardDetailFour =
+      detailedCardSubtitles.planetPopulation;
+    this.collectionsService.cardDetailSix = detailedCardSubtitles.planetClimate;
+  };
+
+  resetDashboard = () => {
+    this.collectionsData = null;
+    this.collectionsService.currenentDisplayedCards = null;
+    this.location.replaceState("");
+  };
+
+  displayDashboardData = event => {
+    const currentData = event.curruentData;
+    this.collectionsService.currenentDisplayedCards = currentData;
+    this.collectionsData = currentData;
+    this.location.replaceState(event.currentPath);
+    this.collectionsService.showDashboardImage = false;
+    this.collectionsService.currentUrl = event.currentPath;
+  };
+
+  checkUrlCurrentState = () => {
+    this.collectionsService.currentUrl
+      ? this.location.replaceState(this.collectionsService.currentUrl)
+      : this.location.replaceState("");
+  };
 }
--- src/app/shared/components/dashboard-card/dashboard-card.component.ts
+++ src/app/shared/components/dashboard-card/dashboard-card.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import { CollectionsServiceService } from "../../services/collections-service.service";
 
 @Component({
-  selector: 'app-dashboard-card',
-  templateUrl: './dashboard-card.component.html',
-  styleUrls: ['./dashboard-card.component.css']
+  selector: "app-dashboard-card",
+  templateUrl: "./dashboard-card.component.html",
+  styleUrls: ["./dashboard-card.component.css"]
 })
 export class DashboardCardComponent implements OnInit {
+  @Input() collectionsData: any;
+  @Input() curruentIndex: number;
+  @Output() onInformationRequest = new EventEmitter();
 
-  constructor() { }
+  curruentFilmIndex: number;
+  data: any;
+
+  detailedCardIdentification = {
+    films: 1,
+    characters: 2,
+    planets: 3
+  };
+
+  constructor(public collectionsService: CollectionsServiceService) {}
 
   ngOnInit(): void {
+    this.collectionsData ? (this.data = this.collectionsData) : null;
+    this.curruentFilmIndex = this.curruentIndex;
   }
 
+  onMoreInformationRequest = () => {
+    if (this.collectionsData["director"]) {
+      this.onInformationRequest.emit({
+        identifyCard: this.detailedCardIdentification.films,
+        cardData: this.data
+      });
+    }
+    if (this.collectionsData["gender"]) {
+      this.onInformationRequest.emit({
+        identifyCard: this.detailedCardIdentification.characters,
+        cardData: this.data
+      });
+    }
+    if (this.collectionsData["climate"]) {
+      this.onInformationRequest.emit({
+        identifyCard: this.detailedCardIdentification.planets,
+        cardData: this.data
+      });
+    }
+  };
 }
