6982be2ca7184d1dcc9f5db168f5f17ae6851c22
Adel  Amr <adelgabreil@adels-mbp.localdomain>
2020-03-14 13:37:18 +0100

    Add relevant routes for navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
-
-const routes: Routes = [];
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { CollectionsDashboardComponent } from "./collections-dashboard/collections-dashboard.component";
+import { DetailsCardComponent } from "./shared/components/details-card/details-card.component";
+const routes: Routes = [
+  { path: "", component: CollectionsDashboardComponent, pathMatch: "full" },
+  { path: "**", redirectTo: "CollectionsDashboardComponent" },
+  { path: "filmDetail/:id", component: DetailsCardComponent },
+  { path: "characterDetail/:id", component: DetailsCardComponent },
+  { path: "planetDetail/:id", component: DetailsCardComponent },
+  { path: "characters", component: CollectionsDashboardComponent },
+  { path: "films", component: CollectionsDashboardComponent },
+  { path: "planets", component: CollectionsDashboardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CollectionsDashboardComponent } from './collections-dashboard/collections-dashboard.component';
-import { DashboardCardComponent } from './shared/components/dashboard-card/dashboard-card.component';
-import { DetailsCardComponent } from './shared/components/details-card/details-card.component';
-import { FurtherDetailedCardComponent } from './shared/components/further-detailed-card/further-detailed-card.component';
-import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { CollectionsDashboardComponent } from "./collections-dashboard/collections-dashboard.component";
+import { NavbarComponent } from "./shared/components/navbar/navbar.component";
+import { DashboardCardComponent } from "./shared/components/dashboard-card/dashboard-card.component";
+import { DetailsCardComponent } from "./shared/components/details-card/details-card.component";
+import { FurtherDetailedCardComponent } from "./shared/components/further-detailed-card/further-detailed-card.component";
+import { HttpClientModule } from "@angular/common/http";
+import { Angular2SwapiModule } from "angular2-swapi";
 
 @NgModule({
   declarations: [
     AppComponent,
     CollectionsDashboardComponent,
+    NavbarComponent,
     DashboardCardComponent,
     DetailsCardComponent,
-    FurtherDetailedCardComponent,
-    NavbarComponent
+    FurtherDetailedCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    Angular2SwapiModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
