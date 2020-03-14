60e64e937422a41cafc4d5f9ae8f925ddb5dfb54
Adel  Amr <adelgabreil@adels-mbp.localdomain>
2020-03-14 13:41:25 +0100

    Add relevant variables to collections service to be used across all application

--- src/app/shared/services/collections-service.service.ts
+++ src/app/shared/services/collections-service.service.ts
@@ -1,9 +1,26 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class CollectionsServiceService {
-
-  constructor() { }
+  genreType: string;
+  subTitleOne: string;
+  subTitleTwo: string;
+  subTitleThree: string;
+  cardDetailOne: string;
+  cardDetailTwo: string;
+  cardDetailThree: string;
+  cardDetailFour: string;
+  cardDetailFive: string;
+  cardDetailSix: string;
+  cardDetailSeven: string;
+  cardDetailEight: string;
+  cardDetailNine: string;
+  currentlySelectedCard: Object;
+  currenentDisplayedCards: any;
+  showDashboardImage = false;
+  id: number = 0;
+  currentUrl: string = "";
+  constructor() {}
 }
